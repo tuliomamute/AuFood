758c1ffb8f32484e686ccd2a5506329973de937d
Túlio Rezende <tulio.mamute@hotmail.com>
2017-05-15 11:26:38 -0300

    Criação da página de produtos

--- Projeto/AuFood/Controllers/HomeController.cs
+++ Projeto/AuFood/Controllers/HomeController.cs
@@ -12,19 +12,9 @@ namespace AuFood.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Products()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View("Products");
         }
     }
 }
